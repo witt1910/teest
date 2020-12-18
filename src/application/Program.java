48f3d76342226c916ce7ba9a1daac00e13cb41c6
witt1910 <wittwizard@gmail.com>
2020-12-18 01:18:49 -0300

    Nova versao

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,33 +10,7 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		int m = sc.nextInt();
-		int n = sc.nextInt();
-		int[][] mat = new int[m][n];
-		
-		for(int i = 0; i < mat.length; i++) {
-			for(int j = 0; j < mat[i].length; j++) {
-				mat[i][j] = sc.nextInt();
-			}
-		}
-
-		int no = sc.nextInt();
-
-		for(int i = 0; i < mat.length; i++) {
-			for(int j = 0; j < mat[i].length; j++) {
-				if(mat[i][j] == no) {
-					System.out.println("Position: " + i + "," + j);
-					if(j > 0)
-						System.out.println("Left: " + mat[i][j-1]);
-					if(i > 0)
-						System.out.println("Up: " + mat[i-1][j]);
-					if(j < mat[i].length - 1)
-						System.out.println("Right: " + mat[i][j + 1]);
-					if(i < mat.length - 1)
-						System.out.println("Down: " + mat[i + 1][j]);
-				}
-			}
-		}
+		System.out.println("Ok");
 		
 		sc.close();
 	}
